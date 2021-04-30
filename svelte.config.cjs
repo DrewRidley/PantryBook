219578ba21b675c838fae1f99659219799ac719c
Drew Ridley <drew.ridley03@gmail.com>
2021-04-30 11:27:50 -0400

    Added tailwind JIT.

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,13 @@
+const preprocess = require("svelte-preprocess");
 /** @type {import('@sveltejs/kit').Config} */
 const vercel = require('@sveltejs/adapter-vercel');
 
 module.exports = {
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
