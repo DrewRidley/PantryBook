bd3fe6106a2f7e68c2621b4853b52e10244e9e50
Drew Ridley <drew.ridley03@gmail.com>
2021-05-03 22:41:54 -0400

    Added some new, experimental authentication behavior. The changes made brought to light the advantages of using SSR, and how to accomplish that with firebase authentication.

---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { auth } from '$lib/fb.js';
+	import { onAuthStateChanged } from 'firebase/auth';
+	import { browser } from '$app/env';
+
+	//The user being updated by the state change.
+	let user;
+	onAuthStateChanged(auth, async (u) => {
+		user = u;
+		console.log("Running on server!");
+	})
+</script>
+
+
+<pre>
+		{JSON.stringify(user, null, '\t')}
+</pre>
+
