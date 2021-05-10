7fabc4022c70cc2704222f6decd6ae701cfe1b2c
Drew Ridley <drew.ridley03@gmail.com>
2021-05-10 09:00:36 -0400

    Fixed vercel build for the hooks.

--- src/hooks.js
+++ src/hooks.js
@@ -1,22 +1,24 @@
 export async function handle({ request, render }) {
 	const { path } = request;
 
-	if (request.headers.cookie.includes('auth')) {
-		if (path == '/') {
+	if (request.headers.cookie != undefined) {
+		if (request.headers.cookie.includes('auth')) {
+			if (path == '/') {
+				return {
+					status: 302,
+					headers: {
+						location: '/dashboard'
+					}
+				};
+			}
+		} else if (path.includes('/dashboard')) {
 			return {
 				status: 302,
 				headers: {
-					location: '/dashboard'
+					location: '/'
 				}
 			};
 		}
-	} else if (path.includes('/dashboard')) {
-		return {
-			status: 302,
-			headers: {
-				location: '/'
-			}
-		};
 	}
 
 	return render(request);
