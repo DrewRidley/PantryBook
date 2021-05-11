c2db670292504693af784b3e763618fef602cb7a
Drew Ridley <drew.ridley03@gmail.com>
2021-05-10 23:36:06 -0400

    Fixed lint. again.

--- src/routes/clients.svelte
+++ src/routes/clients.svelte
@@ -130,13 +130,11 @@
 										</div>
 										<div class="flex-1 flex justify-between sm:justify-end">
 											<div
-
 												class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
 											>
 												Previous
 											</div>
 											<div
-
 												class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
 											>
 												Next
