8d5d76855a29824338c44535fee772b25eab2671
Alexandre Du Lorier <adulorier@gmail.com>
2022-03-20 05:30:13 +0100

    set up material ui

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,28 @@
 <script>
-	import '../app.css';
+  import "../app.css";
+  import "@material/typography/mdc-typography.scss";
+  import Header from "$lib/components/core/nav/Header.svelte";
 </script>
 
+<svelte:head>
+  <!-- Material Icons -->
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  <!-- Roboto -->
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+  />
+  <!-- Roboto Mono -->
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+  />
+  <!-- SMUI Styles -->
+  <link rel="stylesheet" href="/smui.css" />
+</svelte:head>
+
+<Header />
 <slot />
