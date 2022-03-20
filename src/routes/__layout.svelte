d7d425e8d84734acc46d2d1f2ed803889eb6b5af
Alexandre Du Lorier <adulorier@gmail.com>
2022-03-20 06:07:36 +0100

    move links to html template

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,25 +4,5 @@
   import Header from "$lib/components/core/nav/Header.svelte";
 </script>
 
-<svelte:head>
-  <!-- Material Icons -->
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-  />
-  <!-- Roboto -->
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
-  />
-  <!-- Roboto Mono -->
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Roboto+Mono"
-  />
-  <!-- SMUI Styles -->
-  <link rel="stylesheet" href="/smui.css" />
-</svelte:head>
-
 <Header />
 <slot />
